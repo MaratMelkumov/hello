cd08855d153f00b56eff95da231682b1d9e3315f
MaratMelkumov <izdat2000@mail.ru>
2015-12-27 13:29:48 +0300

    init project

---
+++ views/site/say.php
@@ -0,0 +1,7 @@
+<?php
+use yii\helpers\Html;
+?>
+
+<!-- Для обеспечения безопасности мы кодируем пришедшее значение -->
+<h1>Hello <?= Html::encode($target) ?></h1>
+<p>Welcome to your Yii2 demonstration application.</p>
